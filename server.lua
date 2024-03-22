b54aeea66d8aaf3850ac596ac97674b773a9cb99
Mirrrrrow <95571243+Mirrrrrow@users.noreply.github.com>
2024-03-22 01:07:54 +0100

    feat(server): added Exports to manipulate fingerprints

--- server.lua
+++ server.lua
@@ -21,3 +21,54 @@ RegisterServerEvent('fingerprints:server:putMetadata', function (slot)
 
     exports.ox_inventory:SetMetadata(playerId, slot, metadata)
 end)
+
+local function addFingerprint(playerId, slot, identifier)
+    local item = exports.ox_inventory:GetSlot(playerId, slot)
+    if not item then return end
+
+    local metadata = item.metadata or {}
+    metadata.fingerprints = metadata.fingerprints or {
+        users = {},
+        count = 0
+    }
+
+    metadata.fingerprints.count += 1
+    metadata.fingerprints.users[identifier] = true
+
+    exports.ox_inventory:SetMetadata(playerId, slot, metadata)
+end
+
+local function removeFingerprint(playerId, slot, identifier)
+    local item = exports.ox_inventory:GetSlot(playerId, slot)
+    if not item then return end
+
+    local metadata = item.metadata or {}
+    metadata.fingerprints = metadata.fingerprints or {
+        users = {},
+        count = 0
+    }
+
+    if not metadata.fingerprints.users[identifier] then return end
+
+    metadata.fingerprints.count -= 1
+    metadata.fingerprints.users[identifier] = nil
+
+    exports.ox_inventory:SetMetadata(playerId, slot, metadata)
+end
+
+local function purgeFingerprints(playerId, slot)
+    local item = exports.ox_inventory:GetSlot(playerId, slot)
+    if not item then return end
+
+    local metadata = item.metadata or {}
+    metadata.fingerprints = {
+        users = {},
+        count = 0
+    }
+
+    exports.ox_inventory:SetMetadata(playerId, slot, metadata)
+end
+
+exports('AddFingerprint', addFingerprint)
+exports('RemoveFingerprint', removeFingerprint)
+exports('PurgeFingerprints', purgeFingerprints)
